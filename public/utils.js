b440f0c716ef912b339d250c6320ab31fcb58e6c
Beda IGIRANEZA <beda.igiraneza@gmail.com>
2020-04-15 10:42:18 +0200

    first part

---
+++ public/utils.js
@@ -0,0 +1,7 @@
+var utils = {
+    scrollToBottom : function () {
+        if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
+            $('html, body').animate({ scrollTop: $(document).height() }, 0);
+        }
+    }
+};
