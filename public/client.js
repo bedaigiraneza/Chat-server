5c2d48b2d1b0538a83b319324921c651f60dc7ee
Beda IGIRANEZA <beda.igiraneza@gmail.com>
2020-04-15 09:21:00 +0200

    first part

---
+++ public/client.js
@@ -0,0 +1,39 @@
+/*global io*/
+var socket = io();
+
+/**
+ * Envoi d'un message
+ */
+$('#chat form').submit(function (e) {
+    e.preventDefault();
+    var message = {
+        text : $('#m').val()
+    };
+    $('#m').val('');
+    if (message.text.trim().length !== 0) { // Gestion message vide
+        socket.emit('chat-message', message);
+    }
+    $('#chat input').focus(); // Focus sur le champ du message
+});
+
+/**
+ * Connexion d'un utilisateur
+ */
+$('#login form').submit(function (e) {
+    e.preventDefault();
+    var user = {
+        username : $('#login input').val().trim()
+    };
+    if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
+        socket.emit('user-login', user);
+        $('body').removeAttr('id'); // Cache formulaire de connexion
+        $('#chat input').focus(); // Focus sur le champ du message
+    }
+});
+
+/**
+ * Réception d'un message
+ */
+socket.on('chat-message', function (message) {
+    $('#messages').append($('<li>').text(message.text));
+});
